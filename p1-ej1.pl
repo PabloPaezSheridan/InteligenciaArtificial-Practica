3d6bf2e94b467093ce0ada429487b2d41c806515
Pablo Paez <pablopaezps97@gmail.com>
2020-04-10 00:15:09 -0300

    Ejercicios 1 y 2

---
+++ p1-ej1.pl
@@ -0,0 +1,14 @@
+observa(maria,omar).
+observa(laura,omar).
+observa(maria,flavio).
+observa(gabriela,flavio).
+observa(maria,carlos).  
+
+/*observa(maria,flavio).
+ *observa(maria,Quien).
+ *observa(maria,_).
+ *observa(Quien,flavio)
+ *observa(Quien1,Quien2)
+ *observa(_,_).
+
+*/
