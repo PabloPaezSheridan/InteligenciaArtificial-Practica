744f0713e94c3504b8b6044a48d5691f40c1d95c
Pablo Paez <pablopaezps97@gmail.com>
2020-05-18 22:44:56 -0300

    Correciones al feedBack del profesor
    Saqué los or, corregí el 6 y el 4

--- Practica 1/p1-ej3.pl
+++ Practica 1/p1-ej3.pl
@@ -7,8 +7,8 @@ pertenece(nigeria,grupo2).
 pertenece(holanda,grupo2).
 pertenece(escocia,grupo2).
 
-esContrincante(X,Y) :- (pertenece(X,grupo1),pertenece(Y,grupo1));
-                      (pertenece(X,grupo2),pertenece(Y,grupo2)).
+esContrincante(X,Y) :- pertenece(X,grupo1),pertenece(Y,grupo1).
+esContrincante(X,Y) :-  pertenece(X,grupo2),pertenece(Y,grupo2).
                       
 /*
 esContrincante(argentina,brasil).
